f23cf7c0830d6fab00ddaaea60e4e6a46ddf52ba
蔡泽言 <zy_cai@droidhang.com>
2023-06-26 12:11:47 +0800

    射线检测

---
+++ 2D Plateform Test/Assets/Scripts/Controller2D.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Controller2D : MonoBehaviour
+{
+    private const float skinWith = .015f;
+
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+
+    public float horizontalRaySpace = 0;
+    public float verticalRaySpace = 0;
+    
+    private RaycastOrigins raycastOrigins;
+    private BoxCollider2D collider;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+    }
+    
+    
+    //更新顶点
+    void UpdateRaycastOrigins()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWith*-2);
+        
+        raycastOrigins.bottomLeft = bounds.min;
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = bounds.max;
+    }
+
+    //计算间隔
+    void CalculateRaySpacing()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWith*-2);
+        
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, Int32.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, Int32.MaxValue);
+
+        horizontalRaySpace = bounds.size.x / (horizontalRayCount - 1);
+        verticalRaySpace = bounds.size.y / (verticalRayCount - 1);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateRaycastOrigins();
+        CalculateRaySpacing();
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            Debug.DrawRay((raycastOrigins.bottomLeft+horizontalRaySpace*i*Vector2.right),Vector3.up*-2,Color.red);
+        }
+    }
+
+    struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+    
+}
---
+++ 2D Plateform Test/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour
+{
+    private Controller2D controller2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller2D = GetComponent<Controller2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
