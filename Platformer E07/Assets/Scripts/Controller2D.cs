b97d66a48ac8961e9a6c6dced6b469d5a9fddb3f
caizeyan <772373218@qq.com>
2023-07-06 22:27:50 +0800

    添加玩家攀爬系统
    1.当玩家x方向为0 通过玩家之前方向判断其面向是否有墙体
    2.通过玩家x方向与墙体方向判断是哪种爬墙
    3.添加墙体滞留时间，即玩家按多久与墙体相反方向时间，才算真正脱落
    4.判断使用哪种攀爬时，用玩家输入而不是玩家当前移动速度

--- Platformer E07/Assets/Scripts/Controller2D.cs
+++ Platformer E07/Assets/Scripts/Controller2D.cs
@@ -8,23 +8,25 @@ public class Controller2D : RaycastController {
 	
 	public CollisionInfo collisions;
 
-
 	public override void Start() {
 		base.Start ();
-
+		collisions.faceDir = 1;
 	}
 
 	public void Move(Vector3 velocity, bool standingOnPlatform = false) {
 		UpdateRaycastOrigins ();
 		collisions.Reset ();
 		collisions.velocityOld = velocity;
-
+		if (velocity.x != 0)
+		{
+			collisions.faceDir = (int)Mathf.Sign(velocity.x);
+		}
+		
 		if (velocity.y < 0) {
 			DescendSlope(ref velocity);
 		}
-		if (velocity.x != 0) {
-			HorizontalCollisions (ref velocity);
-		}
+		HorizontalCollisions (ref velocity);
+		
 		if (velocity.y != 0) {
 			VerticalCollisions (ref velocity);
 		}
@@ -37,8 +39,12 @@ public class Controller2D : RaycastController {
 	}
 
 	void HorizontalCollisions(ref Vector3 velocity) {
-		float directionX = Mathf.Sign (velocity.x);
+		float directionX = collisions.faceDir;
 		float rayLength = Mathf.Abs (velocity.x) + skinWidth;
+		if (Mathf.Abs(velocity.x) < skinWidth)
+		{
+			rayLength = 2 * skinWidth;
+		}
 		
 		for (int i = 0; i < horizontalRayCount; i ++) {
 			Vector2 rayOrigin = (directionX == -1)?raycastOrigins.bottomLeft:raycastOrigins.bottomRight;
@@ -173,6 +179,8 @@ public class Controller2D : RaycastController {
 		public bool descendingSlope;
 		public float slopeAngle, slopeAngleOld;
 		public Vector3 velocityOld;
+		//玩家面向
+		public int faceDir;
 
 		public void Reset() {
 			above = below = false;
--- Platformer E07/Assets/Scripts/Player.cs
+++ Platformer E07/Assets/Scripts/Player.cs
@@ -10,6 +10,16 @@ public class Player : MonoBehaviour {
 	float accelerationTimeGrounded = .1f;
 	float moveSpeed = 6;
 
+	private float maxWallSlideSpeedMax = 3;
+
+	public Vector2 wallJumpClimb;//上爬
+	public Vector2 wallJumpOff; //离开
+	public Vector2 wallJumpLeap;//斜跳
+
+	public float wallStickTime = .25f;
+	private float timeToWallUnStick = 0;
+	
+	
 	float gravity;
 	float jumpVelocity;
 	Vector3 velocity;
@@ -25,20 +35,73 @@ public class Player : MonoBehaviour {
 		print ("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
 	}
 
-	void Update() {
+	void Update()
+	{
+		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		
+		float targetVelocityX = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+		
+		int wallDirX = controller.collisions.left ? -1 : 1;
+		bool wallSliding = false;
+		if ( (controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0)
+		{
+			wallSliding = true;
+			if (velocity.y < -maxWallSlideSpeedMax)
+			{
+				velocity.y = -maxWallSlideSpeedMax;
+			}
 
+			//设置玩家粘连时间 从按右键开始最少粘连wallStickTime
+			if (timeToWallUnStick > 0)
+			{
+				velocityXSmoothing = 0;
+				velocity.x = 0;
+				if (input.x != wallDirX && input.x != 0)
+				{
+					timeToWallUnStick -= Time.deltaTime;
+				}
+				else
+				{
+					timeToWallUnStick = wallStickTime;
+				}
+			}
+			else
+			{
+				timeToWallUnStick = wallStickTime;
+			}
+		}
+		
+		
 		if (controller.collisions.above || controller.collisions.below) {
 			velocity.y = 0;
 		}
 
-		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
-
-		if (Input.GetKeyDown (KeyCode.Space) && controller.collisions.below) {
-			velocity.y = jumpVelocity;
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (wallSliding)
+			{
+				//注意是input.x
+				if (input.x == wallDirX)
+				{
+					velocity.x = wallJumpClimb.x * -wallDirX;
+					velocity.y = wallJumpClimb.y;
+				}else if (input.x == 0)
+				{
+					velocity.x = wallJumpOff.x * -wallDirX;
+					velocity.y = wallJumpOff.y;
+				}
+				else
+				{
+					velocity.x = wallJumpLeap.x * -wallDirX;
+					velocity.y = wallJumpLeap.y;
+				}
+			}
+			if (controller.collisions.below)
+			{
+				velocity.y = jumpVelocity;
+			}
 		}
-
-		float targetVelocityX = input.x * moveSpeed;
-		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+		
 		velocity.y += gravity * Time.deltaTime;
 		controller.Move (velocity * Time.deltaTime);
 	}
